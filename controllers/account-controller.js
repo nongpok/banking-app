1f6db8cb67dc962b923cbb5cda962c0db1e74915
nongpok <starknonganba@gmail.com>
2022-01-15 12:13:05 +0530

    Implemented Login api

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -13,15 +13,31 @@ module.exports = function (app) {
   });
 
   app.post("/api/v1/account/registration", async (req, res) => {
-    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = new User(
       uuid.v4(),
       req.body.name,
       Number(req.body.balance),
-      hashPassword
+      hashedPassword
     );
     await userService.addUser(user);
     res.json(user);
+  });
+
+  app.post('/api/v1/account/login', async (req, res)=>{
+    const name = req.body.name;
+    const password = req.body.password;
+
+    let user = await userService.getUser(name);
+
+    if(await bcrypt.compare(password, user.password)){
+      console.log("logged in successfully");
+      res.json(user);
+      return;
+    }
+    console.log("Incorrect password");
+    res.status(500).send('Incorrect Password');
+
   });
 
   app.get("/api/v1/account/users", async (req, res) => {
